94156e1f35337dc42e9442eb59855782ffec667b
Lena <developer007.lena@gmail.com>
2025-10-28 06:58:22 -0700

    menu MonthReport YearlyMethod

--- src/accounting/Accounting.java
+++ src/accounting/Accounting.java
@@ -4,6 +4,8 @@
  */
 package accounting;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lena
@@ -15,6 +17,47 @@ public class Accounting {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner scanner=new Scanner(System.in);
+       
+        boolean next=true;
+        printMenu();
+        while(next) {
+         int input=scanner.nextInt();    
+            switch (input) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+            
+            
+          
+        printMenu();  
+        }
+        
+    }
+    
+    public  static void printMenu(){
+        
+        System.out.println("1. Считать все месячные отчеты ");
+        System.out.println("2. Считать годовой отчет ");   
+        System.out.println("3. Сверить отчеты ");
+        System.out.println("4. Вывести информацию о всех месячныйх отчетах ");
+        System.out.println("5. Вывести информацию о  годовом отчетах ");           
+        
     }
     
 }
---
+++ src/accounting/MonthyReport.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accounting;
+
+/**
+ *
+ * @author lena
+ */
+public class MonthyReport {
+    
+    private String itemName;
+    private Boolean isExpense;
+    private  int quantify;
+    private  int sumOfOne;
+    
+    
+    
+}
